142ed9537db11166794067c66711508030081200
Lucas Montecino <lucasmontecino95@gmail.com>
2024-02-24 14:45:38 -0300

    finish-project

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import "./styles.css";
 
+// eslint-disable-next-line react/prop-types
 export default function ImageSlider({ url }) {
   const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
   const [errors, setErrors] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -21,6 +23,26 @@ export default function ImageSlider({ url }) {
     }
   }
 
+  function handleBtnPrev() {
+    if (currentSlide === 0) {
+      setCurrentSlide(images.length - 1);
+    } else {
+      setCurrentSlide(currentSlide - 1);
+    }
+  }
+
+  function handleBtnNext() {
+    if (currentSlide === images.length - 1) {
+      setCurrentSlide(0);
+    } else {
+      setCurrentSlide(currentSlide + 1);
+    }
+  }
+
+  function handleCurrentSlide(getCurrentIndex) {
+    setCurrentSlide(getCurrentIndex);
+  }
+
   useEffect(() => {
     fetchImages(url);
   }, [url]);
@@ -30,26 +52,39 @@ export default function ImageSlider({ url }) {
 
   if (errors !== null) return <p style={{ color: "#E74C3C" }}>{errors}</p>;
 
-  console.log(images);
-
   return (
     <div className="container">
-      <div className="arrow arrow-left">
+      <div className="arrow arrow-left" onClick={handleBtnPrev}>
         <span>«</span>
       </div>
       {images && images.length ? (
-        images.map((img, index) => (
-          <img
-            src={img.url}
-            alt={img.description}
-            key={img.id}
-            className="image-item"
-          />
-        ))
+        images.map(
+          (img, index) =>
+            index === currentSlide && (
+              <img
+                src={img.url}
+                alt={img.description}
+                key={img.id}
+                className="image-item"
+              />
+            )
+        )
       ) : (
         <p>No images to show</p>
       )}
-      <div className="arrow arrow-right">
+      <div className="circle-container">
+        {[...Array(images.length)].map((_, index) => (
+          <div
+            className={`circle ${
+              index === currentSlide ? "active" : "inactive"
+            }`}
+            key={index}
+            onClick={() => handleCurrentSlide(index)}
+          ></div>
+        ))}
+      </div>
+
+      <div className="arrow arrow-right" onClick={handleBtnNext}>
         <span>»</span>
       </div>
     </div>
