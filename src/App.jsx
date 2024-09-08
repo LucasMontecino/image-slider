fd23baadd5c1681f29e6867668a8c634e347f3c3
Lucas Montecino <lucasmontecino95@gmail.com>
2024-09-08 09:44:58 -0300

    fix: useless line of codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import ImageSlider from "./components/image-slider";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <ImageSlider url={"http://localhost:3001/api/images"} />
